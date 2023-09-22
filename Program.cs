80fbb72916c2913c00e3ccbcc931eb4c5672b338
pedrolteixeira <pedrolteixeira66@gmail.com>
2023-09-22 14:06:38 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Calculadora {
+    internal class Program {
+        static void Main (string[] args) {
+            Console.WriteLine("Qual operação você quer fazer?");
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão \n");
+
+            int operacao = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int resultado = 0;
+
+            switch(operacao) {
+                case 1: {
+                    resultado = Adicao(num1, num2);
+                    break;
+                }
+
+                case 2: {
+                    resultado = Subtracao(num1, num2);
+                    break;
+                }
+
+                case 3: {
+                    resultado = Multipliacao(num1, num2);
+                    break;
+                }
+
+                case 4: {
+                    resultado = Divisao(num1, num2);
+                    break;
+                }
+
+                default:
+                    Console.WriteLine("Numero inválido, digite outro número");
+                    break;
+            }
+
+            Console.WriteLine("O resultado da operação é: {0}", resultado);
+        }
+
+        public static int Adicao(int numero1, int numero2) {
+            int result = numero1 + numero2;
+            return result;
+        }
+
+        public static int Subtracao(int numero1, int numero2) {
+            int result = numero1 - numero2;
+            return result;
+        }
+
+        public static int Multipliacao(int numero1, int numero2) {
+            int result = numero1 * numero2;
+            return result;
+        }
+
+        public static int Divisao(int numero1, int numero2) {
+            int result = numero1 / numero2;
+            return result;
+        }
+    }
+}
